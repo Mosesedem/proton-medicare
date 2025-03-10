68e818cfe255af00557b3b3c5564a171a7030acd
Mosesedem <mosesedem81@gmail.com>
2025-03-10 22:29:24 +0100

    feat: update test page content and uncomment NigerianInsuranceDashboard component

--- app/yeah/page.tsx
+++ app/yeah/page.tsx
@@ -1,7 +1,12 @@
 // import NigerianInsuranceDashboard from "@/components/InsuranceDashboard";
 
-// const Index = () => {
-//   return <NigerianInsuranceDashboard />;
-// };
+const Index = () => {
+  return (
+    <div>
+      <p>this is a test page</p>
+    </div>
+  );
+  //   <NigerianInsuranceDashboard />;
+};
 
-// export default Index;
+export default Index;
